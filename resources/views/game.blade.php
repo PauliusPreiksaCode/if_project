70b6cee253b9af7b3a867256a53944b7da71776f
Paulius Preikša <preiksap@gmail.com>
2021-12-16 13:24:45 +0200

    Perkelta ir pakeista css

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -3,75 +3,8 @@
     <head>
         <title>IF projektas</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="/gamePage.css">
-        <style>
-
-            * { /* reset margins and paddings on all elements */
-                margin:0;
-                padding:0;
-            }
-
-            body::before {
-                content:""; /* required for ::before */
-                position: fixed; /* don't scroll with page */
-                z-index:-100; /* place behind elements on page */
-                width:100%;
-                height:100%;
-                background-color:#14C2CC;
-                background-image:
-                    radial-gradient(circle farthest-side at top right, transparent, #0D64FF), radial-gradient(ellipse farthest-corner at 0% 100%, transparent, #FF00A0);
-                animation:bg-change 10s infinite;
-            }
-
-            @keyframes bg-change {
-                0%, 100% {
-                    filter:hue-rotate(0deg);
-                }
-                50% {
-                    filter:hue-rotate(-45deg);
-                }
-            }
-
-            h1{
-                color:red;
-                text-align: center;
-                font-size: 40px;
-            }
-
-            .menu{
-                width: auto;
-                height: 100px;
-                background-color:black;
-                margin: auto;
-                padding-top: 25px;
-            }
-
-            .pavadinimas{
-                padding: 25px;
-                color: black;
-                font-weight: bold;
-                font-family: Arial;
-            }
-
-            .page{
-                width: 80%;
-                margin: auto;
-            }
-
-            p{
-                font-family: Arial;
-                font-size: 20px;
-                text-align: justify;
-                margin: 10px;
-            }
-
-            a:hover{
-                color: white;
-            }
-
-
-        </style>
-      </head>
+        <link rel="stylesheet" href="/css/gamePage.css">
+    </head>
       <body>
       <div class="menu">
           <h1>Meniu juosta</h1>

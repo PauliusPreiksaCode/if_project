f983752ffee65cda91ae254f8c98f3a9e0dbc02b
nedrama <nedaramanauskaite0@gmail.com>
2022-01-18 18:03:50 +0200

    mygtukas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,13 +14,14 @@
             <a href="http://127.0.0.1:8000/game" class = "btn btn-info" role = "button">Zaidimas 2</a>
             <a href="http://127.0.0.1:8000/game" class = "btn btn-info" role = "button">Zaidimas 3</a>
             <a href="http://127.0.0.1:8000/" class = "btn btn-info" role = "button">Apie</a>
-            <div>
-                <button class = "btn btn-info" onclick="Dropdown()">Komanda <i class="fa fa-caret-down"></i></button>
-            <div id="drop">
-                <a href ="http://127.0.0.1:8000/">Arenijus</a>
-                <a href = "http://127.0.0.1:8000/">Neda</a>
-                <a href ="http://127.0.0.1:8000/">Paulius</a>
-            </div>
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Komanda
+                    <span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Paulius Preikša</a></li>
+                    <li><a href="#">Arenijus Juraška</a></li>
+                    <li><a href="#">Neda Ramanauskaitė</a></li>
+                </ul>
             </div>
         </div>
         <div class="background-video">
@@ -31,19 +32,5 @@
                 <h1>IF Projektas</h1>
             </div>
         </div>
-    <script>
-        function Dropdown()
-        {
-            document.getElementById("drop").classList.toggle("show");
-        }
-        window.onclick = function(e) {
-            if (!e.target.matches('.btn btn-info')) {
-                var myDropdown = document.getElementById("drop");
-                if (myDropdown.classList.contains('show')) {
-                    myDropdown.classList.remove('show');
-                }
-            }
-        }
-    </script>
     </body>
 </html>

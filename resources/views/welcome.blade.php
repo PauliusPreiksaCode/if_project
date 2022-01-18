5b47f22cbbfdda3d6468dc02a4f9ce00120d1648
nedrama <nedaramanauskaite0@gmail.com>
2022-01-18 16:17:06 +0200

    mygtukas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>IF-project</title>
         <link rel="stylesheet" href="/css/Main.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     </head>
     <body>
         <div class="menu">

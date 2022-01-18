0b631a9e23806089d9574550e4120cd6ba70bd05
nedrama <nedaramanauskaite0@gmail.com>
2022-01-18 17:53:21 +0200

    mygtukas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,18 @@
     <body>
         <div class="menu">
             <a href="http://127.0.0.1:8000/" class = "btn btn-info" role = "button">Pagrindinis</a>
+            <a href="http://127.0.0.1:8000/game" class = "btn btn-info" role = "button">Zaidimas 1</a>
+            <a href="http://127.0.0.1:8000/game" class = "btn btn-info" role = "button">Zaidimas 2</a>
+            <a href="http://127.0.0.1:8000/game" class = "btn btn-info" role = "button">Zaidimas 3</a>
+            <a href="http://127.0.0.1:8000/" class = "btn btn-info" role = "button">Apie</a>
+            <div>
+                <button class = "btn btn-info" onclick="Dropdown()">Komanda <i class="fa fa-caret-down"></i></button>
+            <div class = "btn btn-info" id="drop">
+                <a href ="http://127.0.0.1:8000/">Arenijus</a>
+                <a href = "http://127.0.0.1:8000/">Neda</a>
+                <a href ="http://127.0.0.1:8000/">Paulius</a>
+            </div>
+            </div>
         </div>
         <div class="background-video">
             <video autoplay muted loop>
@@ -19,5 +31,19 @@
                 <h1>IF Projektas</h1>
             </div>
         </div>
+    <script>
+        function Dropdown()
+        {
+            document.getElementById("drop").classList.toggle("show");
+        }
+        window.onclick = function(e) {
+            if (!e.target.matches('.btn btn-info')) {
+                var myDropdown = document.getElementById("drop");
+                if (myDropdown.classList.contains('show')) {
+                    myDropdown.classList.remove('show');
+                }
+            }
+        }
+    </script>
     </body>
 </html>

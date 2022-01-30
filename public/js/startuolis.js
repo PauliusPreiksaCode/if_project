377978ee01cd194a2989662589ba67c15e5fa614
Paulius Preikša <69482482+PauliusPreiksaCode@users.noreply.github.com>
2022-01-30 20:57:42 +0200

    Pradetas "startuolis", start menu

--- public/js/startuolis.js
+++ public/js/startuolis.js
@@ -40,22 +40,70 @@ Object.defineProperty(exports, "__esModule", ({
 var canvas;
 var ctx;
 
-var Rectangle = /*#__PURE__*/_createClass(function Rectangle(x, y, width, height) {
+var Corner = /*#__PURE__*/_createClass(function Corner(x, y) {
+  _classCallCheck(this, Corner);
+
+  this.x = x;
+  this.y = y;
+});
+
+var Rectangle = /*#__PURE__*/_createClass( //
+function Rectangle(x, y, width, height) {
   var _this = this;
 
   var color = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : "black";
   var lineWidth = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : 1;
+  var text = arguments.length > 6 && arguments[6] !== undefined ? arguments[6] : "";
+  var textColor = arguments.length > 7 && arguments[7] !== undefined ? arguments[7] : "white";
+  var fontSize = arguments.length > 8 && arguments[8] !== undefined ? arguments[8] : 1;
+  var button = arguments.length > 9 && arguments[9] !== undefined ? arguments[9] : false;
 
   _classCallCheck(this, Rectangle);
 
-  this.draw = function () {
+  this.button = false; // for button
+
+  this.down = false;
+
+  this.drawText = function () {
     ctx.save();
     ctx.beginPath();
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = _this.textColor;
+    ctx.font = _this.fontSize + "px Verdana";
+    ctx.fillText(_this.text, _this.x, _this.y);
+    ctx.restore();
+  };
+
+  this.draw = function () {
+    _this.drawText();
+
+    ctx.save();
     ctx.strokeStyle = _this.color;
     ctx.lineWidth = _this.lineWidth;
-    ctx.rect(_this.x, _this.y, _this.width, _this.height);
+
+    if (_this.button) {
+      if (_this.down) {
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(_this.x - _this.width / 2, _this.y - _this.height / 2, _this.width, _this.height);
+
+        _this.drawText();
+      }
+    }
+
+    ctx.rect(_this.x - _this.width / 2, _this.y - _this.height / 2, _this.width, _this.height);
     ctx.stroke();
     ctx.restore();
+  };
+
+  this.mouseDown = function (event) {
+    var x = event.x - canvas.offsetLeft + window.pageXOffset;
+    var y = event.y - canvas.offsetTop + window.pageYOffset;
+    if (x > _this.x - _this.width / 2 && x < _this.x + _this.width / 2 && y > _this.y - _this.height / 2 && y < _this.y + _this.height / 2) _this.down = true;
+  };
+
+  this.mouseUp = function (event) {
+    _this.down = false;
   };
 
   this.x = x;
@@ -64,6 +112,12 @@ var Rectangle = /*#__PURE__*/_createClass(function Rectangle(x, y, width, height
   this.width = width;
   this.lineWidth = lineWidth;
   this.color = color;
+  this.text = text;
+  this.textColor = textColor;
+  this.fontSize = fontSize;
+  this.button = button; // for button
+
+  if (this.button) canvas.addEventListener("mousedown", this.mouseDown, false);
 });
 
 var Circle = /*#__PURE__*/_createClass(function Circle(x, y, radius) {
@@ -71,17 +125,34 @@ var Circle = /*#__PURE__*/_createClass(function Circle(x, y, radius) {
 
   var color = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "black";
   var lineWidth = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 1;
+  var button = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : false;
 
   _classCallCheck(this, Circle);
+
+  this.down = false;
 
   this.draw = function () {
     ctx.save();
     ctx.beginPath();
     ctx.strokeStyle = _this2.color;
     ctx.lineWidth = _this2.lineWidth;
+    ctx.fillStyle = _this2.color;
     ctx.arc(_this2.x, _this2.y, _this2.radius, 0, Math.PI * 2, false);
+    ctx.fill();
     ctx.stroke();
     ctx.restore();
+  };
+
+  this.mouseDown = function (event) {
+    var x = event.x - canvas.offsetLeft + window.pageXOffset;
+    var y = event.y - canvas.offsetTop + window.pageYOffset;
+    x -= _this2.x;
+    y -= _this2.y;
+
+    if (Math.pow(x, 2) + Math.pow(y, 2) < Math.pow(_this2.radius, 2)) {
+      _this2.down = true;
+      clear = true;
+    }
   };
 
   this.x = x;
@@ -89,9 +160,52 @@ var Circle = /*#__PURE__*/_createClass(function Circle(x, y, radius) {
   this.radius = radius;
   this.lineWidth = lineWidth;
   this.color = color;
+  this.button = button;
+  if (this.button) canvas.addEventListener("mousedown", this.mouseDown, false);
+});
+
+var Triangle = /*#__PURE__*/_createClass(function Triangle(x, y, size) {
+  var _this3 = this;
+
+  var color = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "black";
+  var lineWidth = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 1;
+  var button = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : false;
+
+  _classCallCheck(this, Triangle);
+
+  this.corners = new Array();
+
+  this.draw = function () {
+    ctx.save();
+    ctx.beginPath();
+    ctx.strokeStyle = _this3.color;
+    ctx.fillStyle = _this3.color;
+    ctx.lineWidth = _this3.lineWidth;
+    ctx.moveTo(_this3.x + _this3.corners[_this3.corners.length - 1].x, _this3.y + _this3.corners[_this3.corners.length - 1].y);
+
+    for (var i = 0; i < _this3.corners.length; i++) {
+      ctx.lineTo(_this3.x + _this3.corners[i].x, _this3.y + _this3.corners[i].y);
+    }
+
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
+  };
+
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.lineWidth = lineWidth;
+  this.color = color;
+  this.button = button;
+  this.corners.push(new Corner(-1 * size, -1 * size));
+  this.corners.push(new Corner(-1 * size, size));
+  this.corners.push(new Corner(size * 1.3, 0));
 });
 
 var shapes = new Array();
+var clear = false;
 
 function gameLoop() {
   requestAnimationFrame(gameLoop);
@@ -99,9 +213,11 @@ function gameLoop() {
   ctx.fillRect(0, 0, 1280, 720);
   var shape;
 
-  for (var i = 0; i < shapes.length; i++) {
-    shape = shapes[i];
-    shape.draw();
+  if (!clear) {
+    for (var i = 0; i < shapes.length; i++) {
+      shape = shapes[i];
+      shape.draw();
+    }
   }
 }
 
@@ -109,7 +225,9 @@ window.onload = function () {
   canvas = document.getElementById("game"); // @ts-ignore
 
   ctx = canvas.getContext("2d");
-  shapes.push(new Rectangle(100, 100, 100, 100, "black", 5));
+  shapes.push(new Circle(640, 400, 200, "blue", 6, true));
+  shapes.push(new Triangle(640, 400, 100, "white", 3));
+  shapes.push(new Rectangle(640, 100, 500, 140, "white", 1, "Startuolis", "black", 100));
   gameLoop();
 };
 })();
--- resources/js/startuolis.ts
+++ resources/js/startuolis.ts
@@ -10,6 +10,19 @@ interface Shape
     y:number;
     lineWidth:number;
     color:string;
+    button: boolean;
+}
+
+class Corner
+{
+    public x: number;
+    public y: number;
+
+    constructor(x:number, y:number)
+    {
+        this.x = x;
+        this.y = y;
+    }
 }
 
 class Rectangle implements Shape
@@ -20,8 +33,14 @@ class Rectangle implements Shape
     public width: number;
     public lineWidth: number;
     public color: string;
-
-    constructor(x: number, y: number, width:number, height:number, color: string = "black", lineWidth: number = 1)
+    public text: string;
+    public fontSize: number;
+    public textColor: string;
+    public button:boolean = false;
+    // for button
+    public down: boolean = false;
+    //
+    constructor(x: number, y: number, width:number, height:number, color: string = "black", lineWidth: number = 1, text: string = "", textColor: string = "white", fontSize:number = 1, button: boolean = false)
     {
         this.x = x;
         this.y = y;
@@ -29,16 +48,55 @@ class Rectangle implements Shape
         this.width = width;
         this.lineWidth = lineWidth;
         this.color = color;
+        this.text = text;
+        this.textColor = textColor;
+        this.fontSize = fontSize;
+        this.button = button;
+        // for button
+        if(this.button)
+            canvas.addEventListener("mousedown", this.mouseDown, false);
+    }
+
+
+    public drawText = (): void => {
+        ctx.save();
+        ctx.beginPath();
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = this.textColor;
+        ctx.font = this.fontSize + "px Verdana";
+        ctx.fillText(this.text, this.x, this.y);
+        ctx.restore();
     }
 
     public draw = (): void =>{
+        this.drawText();
         ctx.save();
-        ctx.beginPath();
         ctx.strokeStyle = this.color;
         ctx.lineWidth = this.lineWidth;
-        ctx.rect(this.x, this.y, this.width, this.height);
+        if(this.button)
+        {
+            if (this.down) {
+                ctx.fillStyle = "yellow";
+                ctx.fillRect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+                this.drawText();
+            }
+        }
+        ctx.rect(this.x - this.width/2, this.y - this.height/2, this.width, this.height);
         ctx.stroke();
         ctx.restore();
+    }
+
+    public mouseDown = (event: MouseEvent): void => {
+        var x: number = event.x - canvas.offsetLeft + window.pageXOffset;
+        var y: number = event.y - canvas.offsetTop + window.pageYOffset;
+
+        if (x > this.x - this.width / 2 && x < this.x + this.width / 2 && y > this.y - this.height / 2 && y < this.y + this.height / 2)
+            this.down = true;
+    }
+
+    public mouseUp = (event: MouseEvent): void => {
+        this.down = false;
     }
 }
 
@@ -49,14 +107,21 @@ class Circle implements Shape
     public radius: number;
     public lineWidth: number;
     public color: string;
+    public button: boolean;
+    public down: boolean = false;
 
-    constructor(x: number, y: number, radius:number, color: string = "black", lineWidth: number = 1)
+
+    constructor(x: number, y: number, radius:number, color: string = "black", lineWidth: number = 1, button: boolean = false)
     {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.lineWidth = lineWidth;
         this.color = color;
+        this.button = button;
+
+        if(this.button)
+            canvas.addEventListener("mousedown", this.mouseDown, false);
     }
 
     public draw = (): void =>{
@@ -64,31 +129,98 @@ class Circle implements Shape
         ctx.beginPath();
         ctx.strokeStyle = this.color;
         ctx.lineWidth = this.lineWidth;
+        ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+    }
+
+    public mouseDown = (event: MouseEvent): void => {
+        var x: number = event.x - canvas.offsetLeft + window.pageXOffset;
+        var y: number = event.y - canvas.offsetTop + window.pageYOffset;
+        x -= this.x;
+        y -= this.y;
+
+        if(Math.pow(x,2) + Math.pow(y, 2) < Math.pow(this.radius, 2))
+        {
+            this.down = true;
+            clear = true;
+        }
+    }
+}
+
+class Triangle implements Shape
+{
+    public x: number;
+    public y: number;
+    public lineWidth: number;
+    public color: string;
+    public size: number;
+    public corners: Array<Corner> = new Array<Corner>();
+    public button: boolean;
+
+    constructor(x: number, y: number, size:number, color: string = "black", lineWidth: number = 1, button: boolean = false)
+    {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.lineWidth = lineWidth;
+        this.color = color;
+        this.button = button;
+
+        this.corners.push(new Corner(-1 * size, -1 * size));
+        this.corners.push(new Corner(-1 * size, size));
+        this.corners.push(new Corner(size * 1.3, 0));
+    }
+
+    public draw = (): void =>{
+        ctx.save();
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.fillStyle = this.color;
+        ctx.lineWidth = this.lineWidth;
+        ctx.moveTo(this.x + this.corners[this.corners.length-1].x, this.y + this.corners[this.corners.length-1].y);
+        for(var i:number = 0; i < this.corners.length; i++)
+        {
+            ctx.lineTo(this.x + this.corners[i].x, this.y + this.corners[i].y);
+        }
+        ctx.closePath();
+        ctx.fill();
         ctx.stroke();
         ctx.restore();
     }
 }
 
+
+
+
 var shapes: Array<Shape> = new Array<Shape>();
+var clear: boolean = false;
 
 function gameLoop()
 {
     requestAnimationFrame(gameLoop);
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, 1280, 720);
+
     var shape: Shape;
-    for(var i: number = 0; i < shapes.length; i++)
+    if(!clear)
     {
-        shape = shapes[i];
-        shape.draw();
+        for (var i: number = 0; i < shapes.length; i++) {
+            shape = shapes[i];
+            shape.draw();
+        }
     }
 }
 
 window.onload = () => {
     canvas = <HTMLCanvasElement>document.getElementById("game");
+
     // @ts-ignore
     ctx = canvas.getContext("2d");
-    shapes.push(new Rectangle(100, 100, 100, 100, "black", 5));
+    shapes.push(new Circle(640, 400, 200, "blue", 6, true));
+    shapes.push(new Triangle(640, 400, 100, "white", 3));
+    shapes.push(new Rectangle(640, 100, 500, 140, "white", 1, "Startuolis", "black", 100));
     gameLoop();
 }

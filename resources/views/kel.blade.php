eeae8451cb1fbb6d6a16606b7e6eba318675cc33
nedrama <nedaramanauskaite0@gmail.com>
2022-02-16 20:16:59 +0200

    zaidimas -"keliautojas" uzrasai

--- resources/views/kel.blade.php
+++ resources/views/kel.blade.php
@@ -67,11 +67,75 @@
         var player = new Array();
         var tr = new Array();
         var mapEl = new Array();
+        var inv = new Array();
+        var img = new Image();
         window.addEventListener("keydown", function (e) {
             if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(e.code) > -1) {
                 e.preventDefault();
             }
         }, false);
+        var info = /** @class */ (function () {
+            function info(x, y, width, height, text, fontSize, color, textColor) {
+                var _this = this;
+                if (fontSize === void 0) { fontSize = 15; }
+                this.pressed = false;
+                this.gotten = false;
+                this.draw = function () {
+                    ctx.save();
+                    ctx.fillStyle = _this.color;
+                    if (_this.pressed == true) {
+                        ctx.globalAlpha = 0.5;
+                        ctx.fillRect(_this.x - _this.midx - 2, _this.y - _this.midy + 2, _this.width, _this.height);
+                    }
+                    else {
+                        ctx.fillRect(_this.x - _this.midx, _this.y - _this.midy, _this.width, _this.height);
+                    }
+                    ctx.restore();
+                    if (_this.gotten) {
+                        ctx.save();
+                        ctx.beginPath();
+                        ctx.textAlign = "center";
+                        ctx.textBaseline = "middle";
+                        ctx.fillStyle = _this.textColor;
+                        ctx.font = _this.fontSize + "px Verdana";
+                        if (_this.pressed == true) {
+                            ctx.globalAlpha = 0.5;
+                            ctx.fillText(_this.text, _this.x - 2, _this.y + 2);
+                        }
+                        else {
+                            ctx.fillText(_this.text, _this.x, _this.y);
+                        }
+                        ctx.restore();
+                    }
+                };
+                this.clicked = function (event) {
+                    var x = event.x - canvas.offsetLeft;
+                    var y = event.y - canvas.offsetTop + window.pageYOffset;
+                    if (x > _this.x - _this.midx && y > _this.y - _this.midy && x < _this.x + _this.midx && y < _this.y + _this.midy) {
+                        _this.pressed = true;
+                    }
+                };
+                this.not = function (event) {
+                    _this.pressed = false;
+                };
+                this.reset = function () {
+                    _this.gotten = false;
+                };
+                this.x = x;
+                this.y = y;
+                this.width = width;
+                this.height = height;
+                this.color = color;
+                this.midx = width / 2;
+                this.midy = height / 2;
+                this.text = text;
+                this.fontSize = fontSize;
+                this.textColor = textColor;
+                canvas.addEventListener("mousedown", this.clicked, false);
+                canvas.addEventListener("mouseup", this.not, false);
+            }
+            return info;
+        }());
         var romb = /** @class */ (function () {
             function romb(x, y, color) {
                 var _this = this;
@@ -126,7 +190,7 @@
                     ctx.restore();
                 };
                 this.transition = function (x, y, x2, y2) {
-                    if ((x >= _this.x && x <= _this.x + _this.width || x2 >= _this.x && x2 <= _this.x + _this.width) && (y >= _this.y && y <= _this.y + _this.height || y2 >= _this.y && y2 <= _this.y + _this.height)) {
+                    if (((x >= _this.x && x <= _this.x + _this.width) || (x2 >= _this.x && x2 <= _this.x + _this.width) || (x <= _this.x && x2 >= _this.x + _this.width)) && ((y >= _this.y && y <= _this.y + _this.height) || (y2 >= _this.y && y2 <= _this.y + _this.height) || (y <= _this.y && y2 >= _this.y + _this.height))) {
                         _this.isT = true;
                     }
                     return _this.isT;
@@ -246,6 +310,10 @@
                 for (var i = 2; i < mapEl.length; i++) {
                     mapEl[i].color = "green";
                 }
+                for (var i = 0; i < inv.length; i++) {
+                    inv[i].reset();
+                    tr[4 + i].color = "black";
+                }
                 screen0();
             }
             /*if (draw_array[1].pressed == true) {
@@ -291,6 +359,9 @@
                 var d = menuButtons[i];
                 d.draw();
             }
+            if (menuButtons[0].pressed == true) {
+                notes();
+            }
             if (menuButtons[2].pressed == true) {
                 map();
             }
@@ -328,6 +399,18 @@
             mapEl[3].color = "yellow";
             ctx.fillStyle = "blue";
             ctx.fillRect(0, 0, canvas.width, canvas.height);
+            tr[4].draw();
+            if (tr[4].transition(player[0].x, player[0].y, player[0].x + player[0].width, player[0].y + player[0].height)) {
+                inv[1].gotten = true;
+                tr[4].color = "grey";
+                tr[4].isT = false;
+            }
+            tr[6].draw();
+            if (tr[6].transition(player[0].x, player[0].y, player[0].x + player[0].width, player[0].y + player[0].height)) {
+                inv[2].gotten = true;
+                tr[6].color = "grey";
+                tr[6].isT = false;
+            }
             if (menuButtons[3].pressed == true) {
                 cancelAnimationFrame(s1);
                 cancelAnimationFrame(pl);
@@ -345,6 +428,12 @@
             mapEl[4].color = "yellow";
             ctx.fillStyle = "red";
             ctx.fillRect(0, 0, canvas.width, canvas.height);
+            tr[5].draw();
+            if (tr[5].transition(player[0].x, player[0].y, player[0].x + player[0].width, player[0].y + player[0].height)) {
+                inv[0].gotten = true;
+                tr[5].color = "grey";
+                tr[5].isT = false;
+            }
             if (menuButtons[3].pressed == true) {
                 cancelAnimationFrame(s2);
                 cancelAnimationFrame(pl);
@@ -370,12 +459,44 @@
                 var d = mapEl[i];
                 d.drav();
             }
+            if (menuButtons[0].pressed == true) {
+                cancelAnimationFrame(m);
+            }
             if (menuButtons[3].pressed == true) {
                 cancelAnimationFrame(m);
             }
             if (xb[0].pressed == true) {
                 cancelAnimationFrame(m);
             }
+        }
+        function notes() {
+            var n = requestAnimationFrame(notes);
+            ctx.fillStyle = "#33FFE2";
+            ctx.fillRect(canvas.width / 70 * 0.5, canvas.height / 70, canvas.width / 4 * 3.5, canvas.height * 0.9);
+            xb[0].draw();
+            for (var i = 0; i < inv.length; i++) {
+                var d = inv[i];
+                d.draw();
+            }
+            if (menuButtons[2].pressed == true) {
+                cancelAnimationFrame(n);
+            }
+            if (menuButtons[3].pressed == true) {
+                cancelAnimationFrame(n);
+            }
+            if (xb[0].pressed == true) {
+                cancelAnimationFrame(n);
+            }
+            if (inv[0].pressed && inv[0].gotten) {
+                var pic = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Public_Domain_Mark_button.svg/1920px-Public_Domain_Mark_button.svg.png";
+                displayImg(pic, canvas.width / 6 * 2.5, canvas.height / 2, canvas.width / 4 * 3.2, canvas.height * 0.8);
+            }
+        }
+        function displayImg(pic, x, y, width, height) {
+            img.src = pic;
+            x = x - width / 2;
+            y = y - height / 2;
+            ctx.drawImage(img, x, y, width, height);
         }
         function keyboardInput(event) {
             if (event.keyCode == 37 || event.keyCode == 65) {
@@ -409,15 +530,15 @@
             canvas = document.getElementById("canvas");
             canvas.width = window.innerWidth * 0.8;
             canvas.height = canvas.width * 9 / 16;
-            //@ ts-ignore
+            // @ts-ignore
             ctx = canvas.getContext("2d");
             draw_array.push(new Button(canvas.width / 2, canvas.height / 4, canvas.width / 5 * 3, canvas.height / 7 * 2, "Naujas žaidimas", 45, "red", "white"));
-            draw_array.push(new Button(canvas.width / 2, canvas.height / 4 * 3, canvas.width / 5 * 3, canvas.height / 7 * 2, "Tęsti", 50, "blue", "white"));
-            menuButtons.push(new Button(canvas.width / 16 * 15, canvas.height / 14, canvas.width / 12, canvas.height / 14, "Užrašai", canvas.height / 30, "blue", "black"));
-            menuButtons.push(new Button(canvas.width / 16 * 15, canvas.height / 6.2, canvas.width / 12, canvas.height / 14, "Daiktai", canvas.height / 30, "blue", "black"));
-            menuButtons.push(new Button(canvas.width / 16 * 15, canvas.height / 4, canvas.width / 12, canvas.height / 14, "Žemėlapis", canvas.height / 40, "blue", "black"));
-            menuButtons.push(new Button(canvas.width / 16 * 15, canvas.height / 2.98, canvas.width / 12, canvas.height / 14, "Išeiti", canvas.height / 30, "blue", "black"));
-            xb.push(new Button(canvas.width / 4 * 3.45, canvas.height / 20, canvas.height / 14, canvas.height / 14, "x", canvas.height / 10, "red", "white"));
+            draw_array.push(new Button(canvas.width / 2, canvas.height / 4 * 3, canvas.width / 5 * 3, canvas.height / 7 * 2, "Testi", 50, "blue", "white"));
+            menuButtons.push(new Button(canvas.width / 16 * 15, canvas.height / 14, canvas.width / 12, canvas.height / 14, "Užrašai", 25, "blue", "black"));
+            menuButtons.push(new Button(canvas.width / 16 * 15, canvas.height / 6.2, canvas.width / 12, canvas.height / 14, "Daiktai", 25, "blue", "black"));
+            menuButtons.push(new Button(canvas.width / 16 * 15, canvas.height / 4, canvas.width / 12, canvas.height / 14, "Žemėlapis", 20, "blue", "black"));
+            menuButtons.push(new Button(canvas.width / 16 * 15, canvas.height / 2.98, canvas.width / 12, canvas.height / 14, "Išeiti", 25, "blue", "black"));
+            xb.push(new Button(canvas.width / 4 * 3.45, canvas.height / 20, canvas.height / 14, canvas.height / 14, "x", 50, "red", "white"));
             player.push(new Player(canvas.width / 2 * 0.9, canvas.height * 0.8, canvas.width / 9, canvas.width / 9, "black"));
             tr.push(new transition(0, -20, canvas.width, 20, "black"));
             tr.push(new transition(-20, 0, 20, canvas.height, "black"));
@@ -428,6 +549,12 @@
             mapEl.push(new romb(canvas.height, canvas.height / 7));
             mapEl.push(new romb(canvas.height * 0.9, canvas.height / 25));
             mapEl.push(new romb(canvas.height * 1.105, canvas.height / 25));
+            tr.push(new transition(canvas.width / 2, canvas.height / 2, canvas.height / 7, canvas.height / 7, "black"));
+            tr.push(new transition(canvas.width / 5, canvas.height / 5, canvas.height / 7, canvas.height / 7, "black"));
+            tr.push(new transition(canvas.width / 3, canvas.height / 5, canvas.height / 7, canvas.height / 7, "black"));
+            inv.push(new info(canvas.width / 6 * 2.5, canvas.height / 7.5, canvas.width / 4 * 3.2, canvas.height * 0.18, "thfhdrv  vrt drt y yr  rtbftfy fr td rtd brtdbrty b ytr ty", canvas.height / 28, "blue", "white"));
+            inv.push(new info(canvas.width / 6 * 2.5, canvas.height / 7.5 + canvas.height * 0.2, canvas.width / 4 * 3.2, canvas.height * 0.18, "thfhdrv  vrt drt y yr  rtbftfy fr td rtd brtdbrty b ytr ty", canvas.height / 28, "blue", "white"));
+            inv.push(new info(canvas.width / 6 * 2.5, canvas.height / 7.5 + canvas.height * 0.4, canvas.width / 4 * 3.2, canvas.height * 0.18, "thfhdrv  vrt drt y yr  rtbftfy fr td rtd brtdbrty b ytr ty", canvas.height / 28, "blue", "white"));
             gameLoop();
         };
         //# sourceMappingURL=app.js.map
